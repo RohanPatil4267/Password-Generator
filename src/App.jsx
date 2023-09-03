220a4569d59e25c86799e10c480aca5a2436cab4
Rohan Patil <rohanpa4267@gmail.com>
2023-09-03 20:00:38 +0530

    Added Styling

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   const [isNumberAllowed, setNumberAllowed] = useState(false);
   const [isSpecialCharAllowed, setSpecialCharAllowed] = useState(false);
 
-  const passInput = useRef()
+  const passInput = useRef();
 
   const GeneratePassword = useCallback(
     (passLength) => {
@@ -31,41 +31,83 @@ function App() {
     GeneratePassword(passLength);
   }, [passLength, isNumberAllowed, isSpecialCharAllowed]);
 
-  const copyToClipboard = ()=>{
-    navigator.clipboard.writeText(passInput.current.value)
-  }
+  const copyToClipboard = () => {
+    navigator.clipboard.writeText(passInput.current.value);
+  };
 
   return (
-    <div className="flex flex-col justify-center items-center">
-      <input readOnly ref={passInput} value={password} className="bg-transparent border-2" type="text" />
-      <button onClick={copyToClipboard} className="cursor-pointer p-2 border-2">Copy</button>
-      <input
-        type="range"
-        min={8}
-        max={20}
-        value={passLength}
-        id="length"
-        onChange={(e) => {
-          setPassLength(e.currentTarget.value);
-        }}
-      />
-      <label htmlFor="length">Length:{passLength} </label>
-      <input
-        type="checkbox"
-        id="numbers"
-        onChange={() => {
-          setNumberAllowed((prevVal) => !prevVal);
-        }}
-      />
-      <label htmlFor="numbers">Number</label>
-      <input
-        type="checkbox"
-        id="specialChars"
-        onChange={() => {
-          setSpecialCharAllowed((prevVal) => !prevVal);
-        }}
-      />
-      <label htmlFor="specialChars">Special Characters</label>
+    <div
+      className="flex flex-col gap-2
+     bg-customBackgroundColor justify-center 
+     items-center min-h-screen"
+    >
+      <h1
+        className="text-customTextColor font-bold 
+      font-yeseva-One text-xl sm:text-4xl"
+      >
+        Password Generator
+      </h1>
+      <div
+        class="font-josefin-Sans flex flex-col p-6 bg-gray-700
+       bg-opacity-30  border border-gray-200 rounded-lg shadow-2xl"
+      >
+        {/* Read-Only Inputs */}
+        <div className="flex flex-col sm:flex-row gap-1">
+          <input
+            readOnly
+            ref={passInput}
+            value={password}
+            className="border-2 sm:w-[400px] 
+            rounded-md px-4 py-2 text-customTextColor 
+            font-semibold outline-none"
+            type="text"
+          />
+          <button
+            onClick={copyToClipboard}
+            className="cursor-pointer border-2 sm:w-[100px]
+            hover:bg-customPrimaryColor rounded-md
+            px-4 py-2 text-white font-semibold"
+          >
+            Copy
+          </button>
+        </div>
+        {/* Actions Inputs */}
+        <div className="flex flex-col mt-4 gap-1 justify-center items-start ">
+          <div className="flex flex-row gap-2">
+            <input
+              type="range"
+              min={8}
+              max={20}
+              value={passLength}
+              id="length"
+              onChange={(e) => {
+                setPassLength(e.currentTarget.value);
+              }}
+            />
+            <label htmlFor="length">Length: {passLength} </label>
+          </div>
+          <div className="flex flex-row gap-2">
+            <input
+              type="checkbox"
+              id="numbers"
+              onChange={() => {
+                setNumberAllowed((prevVal) => !prevVal);
+              }}
+            />
+            <label htmlFor="numbers">Number</label>
+          </div>
+          <div className="flex flex-row gap-2">
+            <input
+              type="checkbox"
+              id="specialChars"
+              onChange={() => {
+                setSpecialCharAllowed((prevVal) => !prevVal);
+              }}
+            />
+            <label htmlFor="specialChars">Special Characters</label>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,11 +2,19 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    fontFamily: {
-      "yeseva-One": ["Yeseva One", "sans-serif"],
-      "josefin-Sans": ["Josefin Sans", "sans-serif"],
+    extend: {
+      fontFamily: {
+        "yeseva-One": ["Yeseva One", "sans-serif"],
+        "josefin-Sans": ["Josefin Sans", "sans-serif"],
+      },
+      colors: {
+        customTextColor: "#2193c4",
+        customBackgroundColor: "#a3b3f0",
+        customPrimaryColor: "#73a8e7",
+        customSecondaryColor: "#80e4ea",
+        customAccentColor: "#55e2c6",
+      },
     },
-    extend: {},
   },
   plugins: [],
 };
