fdcdd475f4a83a3a5058bec52ab00c13b6d7f7b2
Rohan Patil <rohanpa4267@gmail.com>
2023-09-03 14:42:09 +0530

    Basic HTML Skeletal Structure for Password Generator

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,15 @@
 function App() {
   return (
-    <h1>hello</h1>
+    <div className="flex flex-col justify-center items-center">
+      <input className="bg-transparent border-2" type="text" />
+      <button className="cursor-pointer p-2 border-2">Copy</button>
+      <input type="range" id="length" />
+      <label htmlFor="length">Length: </label>
+      <input type="checkbox" id="numbers" />
+      <label htmlFor="numbers">Number</label>
+      <input type="checkbox" id="specialChars" />
+      <label htmlFor="specialChars">Special Characters</label>
+    </div>
   );
 }
 
