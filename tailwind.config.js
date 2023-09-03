1e95b96d83e7688a25b56e357137ec696bc6c7d0
Rohan Patil <rohanpa4267@gmail.com>
2023-09-03 18:33:20 +0530

    Added some custom fonts

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    fontFamily: {
+      "yeseva-One": ["Yeseva One", "sans-serif"],
+      "josefin-Sans": ["Josefin Sans", "sans-serif"],
+    },
     extend: {},
   },
   plugins: [],
-}
+};
